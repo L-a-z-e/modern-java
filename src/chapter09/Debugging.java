ee48bc35258daa23eae765776c95013b4645a6ef
L-a-z-e <yysi8771@gmail.com>
2025-08-10 16:36:29 +0900

    chapter09 - 1

---
+++ src/chapter09/Debugging.java
@@ -0,0 +1,41 @@
+package chapter09;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Debugging {
+    public static void main(String[] args) {
+        List<Point> points = Arrays.asList(new Point(12, 2), null);
+        points.stream().map(Point::getX).forEach(System.out::println);
+//        points.stream().map(Point::getX).forEach(
+//        p -> {
+//            try {
+//                System.out.println(p.getX());
+//            } catch (NullPointerException e) {
+//                System.out.println("Null point detected");
+//            }
+//        }
+//        );
+    }
+
+
+    private static class Point {
+
+        private int x;
+        private int y;
+
+        private Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+    }
+}
