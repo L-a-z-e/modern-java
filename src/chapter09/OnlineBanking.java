0b919036b024a044b555f9d5b1fb4d238df893cb
L-a-z-e <yysi8771@gmail.com>
2025-08-16 20:39:42 +0900

    chapter09 - 2

---
+++ src/chapter09/OnlineBanking.java
@@ -0,0 +1,16 @@
+package chapter09;
+
+abstract class OnlineBanking {
+    public void ProcessCustomer(int id) {
+        Customer c = Database.getCustomerWithId(id);
+        makeCustomerHappy(c);
+    }
+
+    abstract void makeCustomerHappy(Customer c);
+
+    static private class Customer {}
+
+    static private class Database {
+        static Customer getCustomerWithId(int id) { return new Customer(); }
+    }
+}
